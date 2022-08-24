7dbace409848fa24b1cb008f9d9453be76c654df
rszczuka <rszczuka@sii.pl>
2022-08-24 07:54:28 +0200

    added apps libs and test matcher

--- apps/unit-tested-app/src/test-setup.ts
+++ apps/unit-tested-app/src/test-setup.ts
@@ -1,1 +1,27 @@
 import 'jest-preset-angular/setup-jest';
+
+declare global {
+    namespace jest {
+      interface Matchers<R, T> {
+        isElo(): R;
+      }
+    }
+  }
+  
+  const matchers = {
+      isElo(
+        // a matcher is a method, it has access to Jest context on `this`
+        this: jest.MatcherContext,
+        received: string,
+      ) {
+        return {
+          pass: typeof received === "string" && received === "elo",
+          message: () =>
+            `Received string "${received}" does ${
+              this.isNot ? "" : "not "
+            }equal "elo".`,
+        };
+      },
+  };
+  
+  expect.extend(matchers);
